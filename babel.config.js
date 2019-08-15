a0033894648e05382b5b09f31e7e2676a0fd076d
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-08-16 01:53:14 +0900

    init build config

--- babel.config.js
+++ babel.config.js
@@ -4,11 +4,6 @@ module.exports = {
       "@babel/preset-env",
       {
         modules: false,
-        targets: {
-          browsers: "> 1%",
-          ie: 11,
-          uglify: true
-        },
         useBuiltIns: "entry"
       }
     ]
