cbc0f439af7630fe77727fecef14ecc2a916af5e
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-08-20 22:17:02 +0900

    improve demo:build

--- demo-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SRC_FILE="node_modules/vue-cli-plugin-p11n/lib/demo/service.js"
-TMP_FILE="node_modules/vue-cli-plugin-p11n/lib/demo/_service.js"
-
-mv $SRC_FILE $TMP_FILE
-sed -e "s/'serve', args/'build', { ...args, dest: '.\/demo\/dist' }/" $TMP_FILE > $SRC_FILE
-yarn demo
-mv -f $TMP_FILE $SRC_FILE
